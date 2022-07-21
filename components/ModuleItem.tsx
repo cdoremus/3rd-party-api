f2c62676c284d8221989742725acc0b505d9cf4a
Craig Doremus <cdoremus@gmail.com>
2022-07-20 20:29:11 -0400

    Added link to deno.land/x url

--- components/ModuleItem.tsx
+++ components/ModuleItem.tsx
@@ -12,6 +12,6 @@ export default function ModuleItem(props: ModuleItemProps) {
   const module = props.module;
   const rank = props.rank;
   return (
-    <li key={rank} class={tw`text-lg`}><span>{rank}. </span><span class={tw`font-bold`}>{module.name} </span>({module.popularity_score}): {module.description}</li>
+    <li key={rank} class={tw`text-lg`}><span>{rank}. </span><a href={`https://deno.land/x/${module.name}`} target="_blank"><span class={tw`font-bold`}>{module.name} </span></a>({module.popularity_score}): {module.description}</li>
   )
 }
