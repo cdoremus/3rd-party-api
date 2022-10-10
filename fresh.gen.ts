b50770aaba73cfe6e93849b78fb7d93793f6004c
Craig Doremus <cdoremus@gmail.com>
2022-10-10 18:39:10 -0400

    Added module route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,6 +8,7 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/about.tsx";
 import * as $3 from "./routes/api/3rd-party.ts";
 import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/module.tsx";
 import * as $$0 from "./islands/MenuLink.tsx";
 
 const manifest = {
@@ -17,6 +18,7 @@ const manifest = {
     "./routes/about.tsx": $2,
     "./routes/api/3rd-party.ts": $3,
     "./routes/index.tsx": $4,
+    "./routes/module.tsx": $5,
   },
   islands: {
     "./islands/MenuLink.tsx": $$0,
