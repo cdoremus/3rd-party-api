f347d92255fc69ab61f36bb56e8b09c5bedb39d9
Craig Doremus <cdoremus@gmail.com>
2022-10-10 18:42:17 -0400

    Added ModuleDetails type

--- types.ts
+++ types.ts
@@ -6,3 +6,18 @@ export type Module = {
   latest_version: string;
   star_count: number;
 }
+
+export type ModuleDetails = {
+  popularity: {
+    prev_sessions_30_day: number;
+    score: number;
+    prev_score: number;
+    prev_users_30_day: number;
+    sessions_30_days: number;
+    users_30_day: number;
+  }
+
+}
+export type ThirdPartyApi = {
+  items: Module[];
+}
