a898850c3ca41ecc0785c5d67e1a470c2c99a995
Craig Doremus <cdoremus@gmail.com>
2023-04-04 11:35:56 -0400

    Added metrics to Module type

--- types.ts
+++ types.ts
@@ -5,9 +5,10 @@ export type Module = {
   versions?: Array<string>;
   latest_version: string;
   star_count: number;
+  metrics?: ModuleMetrics;
 }
 
-export type ModuleDetails = {
+export type ModuleMetrics = {
   popularity: {
     prev_sessions_30_day: number;
     score: number;
@@ -15,6 +16,9 @@ export type ModuleDetails = {
     prev_users_30_day: number;
     sessions_30_days: number;
     users_30_day: number;
+  },
+  info: {
+    uploaded_at: string; // ISO 8601 format date (see Date.toISOString and std/datetime)
   }
 }
 export type ThirdPartyApi = {
